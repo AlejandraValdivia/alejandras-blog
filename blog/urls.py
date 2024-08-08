9ad95ec22c946d2c7a3e34f098f362a308ea36e4
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-07 18:24:27 -0700

    feat: add page templates

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views # Import views to connect routes to view functions
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('posts', views.posts, name='posts-page'),
+    path('posts/<slug:slug>', views.post_detail, name='post-detail-page'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
 
 # Create your views here.
+def homepage(request):
+    if request is None:
+        raise ValueError("Request object is None")
+    return render(request, 'index.html') 
+
+def posts(request):
+    pass
+
+def post_detail(request):
+    pass
+
+
+
