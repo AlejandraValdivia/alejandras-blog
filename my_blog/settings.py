3094cf61123cfd5bec626c281fb342556d3c37e7
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-10 15:30:01 -0700

    feat: add allowed hosts in settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -32,8 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 ALLOWED_HOSTS = [
-    "", 
-    "",
+    "alejandras-blog.onrender.com", 
+    "https://alejandras-blog.onrender.com",
 ]
 
 
