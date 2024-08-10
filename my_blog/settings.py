6702afed0dd9779ae5bf7ac6de4ac5e621950298
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-10 16:45:37 -0700

    feat: deleted media urls in settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -133,18 +133,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
