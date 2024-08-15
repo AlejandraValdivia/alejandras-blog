190556ca59a40a76631fd83473bc7fc400b472ce
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-14 20:50:46 -0700

    Applied migrations and prepared for deployment

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -100,15 +100,15 @@ DATABASES = {
     )
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'blogdb',
-#         'USER': 'alex',
-#         'PASSWORD': 'myblog',
-#         'HOST': 'localhost'
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogdb',
+        'USER': 'alex',
+        'PASSWORD': 'myblog',
+        'HOST': 'localhost'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
