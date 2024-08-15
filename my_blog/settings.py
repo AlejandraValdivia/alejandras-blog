058ab7bf7ae02b8e7924bbc694e2a0581014c80f
Alejandra Valdivia <alexvcodes@gmail.com>
2024-08-15 14:14:40 -0700

    feat: edit settings.py to read DATABASES content through environment variables

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -181,7 +181,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
